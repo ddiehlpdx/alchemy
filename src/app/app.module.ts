051855f358b64d7b9cb0c4acb9880a5b737b55c9
ddiehlpdx <ddiehl.pdx@gmail.com>
2019-07-01 17:39:27 -0700

    Import HttpClientModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,15 +32,19 @@ import {
   MatToolbarModule,
   MatTooltipModule
 } from '@angular/material';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     MatBadgeModule,
     MatButtonModule,
     MatButtonToggleModule,
